29a66aeae4a5f894bffcc67b1486344a9ee3d7c2
Vinícius Oliveira de Carvalho <viniolicar2004@gmail.com>
2023-03-22 12:51:10 -0300

    Parte de login

--- index.js
+++ index.js
@@ -1,41 +1,72 @@
-const names = document.getElementById("name");
-const email = document.getElementById("email");
-const telephone = document.getElementById("telephone");
-const password = document.getElementById("password");
-const confirmPassword = document.getElementById("confirmPassword");
+// SignUp form fields
+const signupName = document.getElementById("signupName");
+const signupEmail = document.getElementById("signupEmail");
+const signupTelephone = document.getElementById("signupTelephone");
+const signupPassword = document.getElementById("signupPassword");
+const signupConfirmPassword = document.getElementById("signupConfirmPassword");
+const signUpBtn = document.querySelector("#signUpBtn");
+
+// Login form fields
+const loginEmail = document.getElementById("loginEmail");
+const loginPassword = document.getElementById("loginPassword");
+const loginBtn = document.querySelector("#loginBtn");
+
+// Others
 const notification = document.querySelector(".notificationContainer");
-const signUpBtn = document.querySelector(".signUpBtn");
+const otherSide = document.querySelector(".otherSide");
 
 const createAccount = () => {
   const user = {
-    name: names.value,
-    email: email.value,
-    telephone: telephone.value,
-    password: password.value,
-    confirmPassword: confirmPassword.value,
+    name: signupName.value,
+    email: signupEmail.value,
+    telephone: signupTelephone.value,
+    password: signupPassword.value,
+    confirmPassword: signupConfirmPassword.value,
   };
 
   formVerification(user);
 };
 
+const login = () => {
+  const userLogin = {
+    email: loginEmail.value,
+    password: Number(loginPassword.value),
+  };
+
+  const userTest = {
+    email: "viniolicar2004@gmail.com",
+    password: 123456,
+  };
+
+  if (
+    userLogin.email === userTest.email &&
+    userLogin.password === userTest.password
+  ) {
+    // Substituir a mensagem por uma função de login
+    messages("Login correto", "success");
+  } else {
+    messages("Usuário ou senha incorretos", "error");
+  }
+};
+
 const formVerification = (user) => {
   if (user.password != user.confirmPassword) {
-    password.classList.add("invalid");
-    confirmPassword.classList.add("invalid");
+    signupPassword.classList.add("invalid");
+    signupConfirmPassword.classList.add("invalid");
     messages("A senhas não batem", "error");
   } else if (user.password.length < 6) {
-    password.classList.add("invalid");
+    signupPassword.classList.add("invalid");
     messages("Sua senha deve possuir ao menos 6 caracteres", "error");
   } else {
-    confirmPassword.classList.remove("invalid");
-    password.classList.remove("invalid");
+    signupConfirmPassword.classList.remove("invalid");
+    signupPassword.classList.remove("invalid");
   }
 
   if (user.telephone.length < 16) {
-    telephone.classList.add("invalid");
+    signupTelephone.classList.add("invalid");
     messages("Adicione um telefone válido", "error");
   } else {
-    telephone.classList.remove("invalid");
+    signupTelephone.classList.remove("invalid");
   }
 };
 
@@ -57,13 +88,13 @@ const messages = (message, status) => {
   }, 3000);
 };
 
-const formValidation = () => {
+const signupFormValidation = () => {
   if (
-    names.value === "" ||
-    email.value === "" ||
-    telephone.value === "" ||
-    password.value === "" ||
-    confirmPassword.value === ""
+    signupName.value === "" ||
+    signupEmail.value === "" ||
+    signupTelephone.value === "" ||
+    signupPassword.value === "" ||
+    signupConfirmPassword.value === ""
   ) {
     signUpBtn.setAttribute("disabled", true);
   } else {
@@ -71,8 +102,29 @@ const formValidation = () => {
   }
 };
 
+const loginFormValidation = () => {
+  if (loginEmail.value === "" || loginPassword.value === "") {
+    loginBtn.setAttribute("disabled", true);
+  } else {
+    loginBtn.removeAttribute("disabled");
+  }
+};
+
 const telephoneMask = () => {
-  telephone.value = telephone.value.replace(/\D/g, "");
-  telephone.value = telephone.value.replace(/^(\d{2})(\d)(\d+)/, "($1) $2 $3");
-  telephone.value = telephone.value.replace(/(\d{4})(\d)/, "$1-$2");
+  signupTelephone.value = signupTelephone.value.replace(/\D/g, "");
+  signupTelephone.value = signupTelephone.value.replace(
+    /^(\d{2})(\d)(\d+)/,
+    "($1) $2 $3"
+  );
+  signupTelephone.value = signupTelephone.value.replace(/(\d{4})(\d)/, "$1-$2");
+};
+
+const formChange = (direction) => {
+  if (direction === "left") {
+    otherSide.classList.remove("right");
+    otherSide.classList.add("left");
+  } else {
+    otherSide.classList.remove("left");
+    otherSide.classList.add("right");
+  }
 };
